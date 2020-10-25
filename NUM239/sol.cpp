04ba543a4245a28b0bf3753bb1444348818a43bb
chauhan739 <chauhan.aryansingh18@gmail.com>
2020-10-25 10:16:14 +0530

    First Commit

---
+++ NUM239/sol.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+  
+  int n;
+  cin >> n;
+  while(n != 0){
+    
+    int x, y, count = 0;
+    cin >> x >> y;
+    for(int i = x;i <= y;++i){
+      int a = i % 10;
+      if(a == 2 || a == 3 || a == 9){
+        count += 1;
+      }
+    }
+    cout << count << endl;
+    --n;
+  }
+  
+  return 0;
+}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This repository will contain
 some questions' solutions which are
 in CodeChef.
 
-Aby suggestions will be most welcomed.
+Any suggestions will be most welcomed.
